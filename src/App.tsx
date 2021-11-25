1ba7b74cfc43bd6d7e9095450f807f4eb8c1de7c
andrewrgabb <agab5848@uni.sydney.edu.au>
2021-11-26 08:56:18 +1100

    Small test name change

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Andrew Gabb's Apra App</p>
+        <p>Andrew Gabb's Apra Photos App</p>
         <a
           className="App-link"
           href="https://reactjs.org"
