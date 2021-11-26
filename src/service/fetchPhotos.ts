6429c65622429a924ea9cbf060d65a71f7a2d22e
andrewrgabb <agab5848@uni.sydney.edu.au>
2021-11-26 11:58:38 +1100

    Add fetch photos

---
+++ src/service/fetchPhotos.ts
@@ -0,0 +1,63 @@
+// Types
+
+export type Photo = {
+  id: string;
+  title: string;
+  url: string;
+};
+
+export type Photos = {
+  meta: {
+    totalCount: number;
+  };
+  data: Photo[];
+};
+
+type Body = {
+  data: {
+    photos: Photos;
+  };
+};
+
+const API = "https://graphqlzero.almansi.me/api";
+
+export const fetchPhotos = async (): Promise<Photos> => {
+  const query = `
+  query($options: PageQueryOptions) {
+    photos(options: $options) {
+      meta {
+        totalCount
+      }
+      data {
+        id
+        title
+        url
+      }
+    }
+  }`;
+
+  const variables = {
+    options: { search: { q: "officia" }, paginate: { page: 1, limit: 4 } },
+  };
+
+  const queryString =
+    "?query=" + query + "&variables=" + JSON.stringify(variables);
+
+  const photos = await fetch(API + queryString, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(async (res: Response) => {
+      const body: Body = await res.json();
+
+      const photos: Photos = body.data.photos;
+      return photos;
+    })
+    .catch((e: any) => {
+      throw e;
+    });
+
+  return photos;
+};
